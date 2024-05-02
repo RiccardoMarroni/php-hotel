e5d9a75551b73ef3363198bd7ef39a47e4dee7b3
RiccardoMarroni <marroni.riccardo@gmail.com>
2024-05-02 21:01:13 +0200

    exercise base

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+include __DIR__ . "/Models/hotelList.php";
+
+
+$parking = isset($_GET['parking']) ? $_GET['parking'] : null;
+$hotels = array_filter($hotels, function ($hotel) use ($parking) {
+    return $parking ? $hotel['parking'] : true;
+});
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<main>
+        <form action="" method="GET" class="bg-dark text-white container">
+        </form>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">name</th>
+                    <th scope="col">description</th>
+                    <th scope="col">parking</th>
+                    <th scope="col">vote</th>
+                    <th scope="col">distance_to_center</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($hotels as $hotel) {
+                    echo "<tr>";
+                    echo "<th scope='row'>" . $hotel['name'] . "</th>";
+                    echo "<td>" . $hotel['description'] . "</td>";
+                    echo "<td>" . ($hotel['parking'] ? 'Sì' : 'No') . "</td>";
+                    echo "<td>" . $hotel['vote'] . "</td>";
+                    echo "<td>" . $hotel['distance_to_center'] . " km</td>";
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+    </main>
+</body>
+</html>
